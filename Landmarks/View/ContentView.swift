058405afd4e84586ca7fb17ab4dfaa58e66d81c0
Olivier Van hamme <nicefiction@gmail.com>
2022-04-29 12:13:09 +0200

    Composes the Detail view.

--- Landmarks/View/ContentView.swift
+++ Landmarks/View/ContentView.swift
@@ -16,18 +16,31 @@ struct ContentView: View {
     // MARK: - HELPER METHODS
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300.0)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
