6d789bd7a2cb82bf5e522c3e7190b9a1d2ff1bd3
Olivier Van hamme <nicefiction@gmail.com>
2022-04-29 11:21:05 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Olivier Van hamme on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
