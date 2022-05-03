7509518edf3c65b33c07b9f0e757a0155e0d2acd
Olivier Van hamme <nicefiction@gmail.com>
2022-05-03 11:30:49 +0200

    Customises View transitions.

--- Landmarks/Model/Extension/AnyTransitionExtension.swift
+++ Landmarks/Model/Extension/AnyTransitionExtension.swift
@@ -8,7 +8,12 @@ extension AnyTransition {
     
     static var moveAndFade: AnyTransition {
         .asymmetric(
+            /// Switch to using the `move(edge:)` transition,
+            /// so that the graph slides in and out from the same side.
             insertion: .move(edge: .trailing).combined(with: .opacity),
+            /// Use the `asymmetric(insertion:removal:)` modifier
+            /// to provide different transitions
+            /// for when the view appears and disappears.
             removal: .scale.combined(with: .opacity)
         )
     }
