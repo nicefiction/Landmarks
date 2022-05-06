6faa903642e1c4aafd35eeb9de561885081b2261
Olivier Van hamme <nicefiction@gmail.com>
2022-05-06 09:49:21 +0200

    Adds a watchOS target.

---
+++ WatchLandmarks WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Olivier Van hamme on 06/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
